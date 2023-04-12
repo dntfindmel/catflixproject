71cca6248960171009f65e2eed9c463e8bb26e8e
Melyssa Moya <120610288+dntfindmel@users.noreply.github.com>
2023-04-11 23:39:54 -0300

    unfinished

---
+++ frontend/assets/js/header.js
@@ -0,0 +1,21 @@
+//Código pro menu dropdown!
+var dropdownBtn = document.getElementById('dropdownBtn'); //Armazenando o botão do menu na variável.
+var dropdownEl = document.getElementById('dropdownMenu'); //Armazenando o menu dropdown na variável.
+
+//Adicionando um evento de click no botão do menu dropdown pra trocar o display da div com os botões.
+dropdownBtn.addEventListener('click', function() {
+
+    if (dropdownEl.style.display === 'block'){
+    dropdownEl.style.display = 'none'
+    } else {
+        dropdownEl.style.display = 'block'
+    }
+});
+
+//Adicionando um evento pra caso o menu dropdown estiver em uma situação diferente da função acima, verificar e mudar para display none.
+document.addEventListener('click', function(event) {
+    if (!dropdownEl.contains.event.target && event.target !== dropdownBtn) {
+        dropdownContainer.style.display = "none";
+    }
+})
+
